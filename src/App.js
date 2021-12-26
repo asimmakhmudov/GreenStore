b742fbf597846e8dd804d6ac98684373891b6230
asimmakhmudov <asimmahmudov18@gmail.com>
2021-12-26 21:38:01 +0400

    add function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Popup from './components/Popup';
-
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="App">
-      <Popup/>
+    <div className="App" style={{zIndex: "0"}}>
+      <Button style={{margin: "20px"}} variant='success' onClick={togglePopup}>Add to card</Button>
+      {isOpen && <Popup handleClose={togglePopup}/>}
     </div>
   );
 }
